02c6be9d7205643e953749803aa629b5d62a1c26
dannykhant <dannypmkhant@gmail.com>
2026-08-21 22:21:07 +0700

    feat: run2 to be more verbose

--- run2DownloadBaselines.sh
+++ run2DownloadBaselines.sh
@@ -6,8 +6,10 @@ baseline_path="${root_path}/baselines"
 mkdir -p ${baseline_path}
 
 cd ${baseline_path}
+echo '-------------------<< Downloading R-Bot >>-------------------'
 rm -rf R-Bot
 git clone --depth 1 https://github.com/dannykhant/r-bot.git R-Bot
 
+echo '-------------------<< Downloading ReSequel >>-------------------'
 rm -rf ReSequel
 git clone --depth 1 https://github.com/dannykhant/resequel.git ReSequel
