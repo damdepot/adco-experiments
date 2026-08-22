0f4d00d9a970e10a4b6cd935b01199b32b45bf87
dannykhant <dannypmkhant@gmail.com>
2026-08-22 13:06:07 +0700

    chore: r-bot script name changed

--- run8Experiments-R-Bot.sh
+++ run8Experiments-R-Bot.sh
@@ -5,8 +5,12 @@ set -e
 rm -rf results/r-bot
 mkdir -p results/r-bot
 
+rm -rf R-Bot-results
+mkdir -p R-Bot-results
+
 export iteration=5
 export result_dir="results/r-bot"
+export result_output_path="R-Bot-results"
 
 CMDBaselineRBot=./explocal/exp2_Baselines/runExperiment2-R-Bot.sh
 CMDRunRBotVerify=./explocal/exp2_Baselines/runExperiment2-Workload-Verify-R-Bot.sh
@@ -27,4 +31,4 @@ $CMDRunRBotVerify stats-lite PostgreSQL ${model}
 ### Run Stats R-Bot workload
 #**************************
 echo "-------------------<< Running Stats-Lite R-Bot workload >>-------------------"
-$CMDRunWorkload stats-lite PostgreSQL R-Bot-Gemini
+$CMDRunWorkload stats-lite PostgreSQL ${model}
