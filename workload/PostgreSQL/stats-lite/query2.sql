f5c4ae0f0de5f0500a8ff2639cfd7e8c5dfb3602
dannykhant <dannypmkhant@gmail.com>
2026-08-23 19:21:08 +0700

    chore: complex stats-lite

--- workload/PostgreSQL/stats-lite/query2.sql
+++ workload/PostgreSQL/stats-lite/query2.sql
@@ -2,8 +2,27 @@ SELECT
     COUNT(*)
 FROM
     comments as c,
-    badges as b
+    posts as p,
+    postLinks as pl,
+    postHistory as ph,
+    votes as v,
+    badges as b,
+    users as u
 WHERE
-    c.UserId = b.UserId
+    p.Id = pl.RelatedPostId
+    AND b.UserId = u.Id
+    AND c.UserId = u.Id
+    AND p.Id = v.PostId
+    AND p.Id = c.PostId
+    AND p.Id = ph.PostId
     AND c.Score = 0
-    AND b.Date <= '2014-09-11 14:33:06' :: timestamp;
+    AND c.CreationDate >= '2010-07-26 17:09:48' :: timestamp
+    AND p.PostTypeId = 1
+    AND p.AnswerCount >= 0
+    AND p.CommentCount >= 0
+    AND p.CommentCount <= 14
+    AND pl.CreationDate >= '2010-10-27 10:02:57' :: timestamp
+    AND pl.CreationDate <= '2014-09-04 17:23:50' :: timestamp
+    AND ph.CreationDate <= '2014-09-11 20:09:41' :: timestamp
+    AND v.CreationDate >= '2010-07-21 00:00:00' :: timestamp
+    AND v.CreationDate <= '2014-09-14 00:00:00' :: timestamp;
