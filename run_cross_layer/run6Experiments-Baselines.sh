c0353f57ab61ec24beba42317d24a6862d5837cb
dannykhant <dannypmkhant@gmail.com>
2026-08-30 12:59:07 +0700

    feat: smallback output in results dir

--- run_cross_layer/run6Experiments-Baselines.sh
+++ run_cross_layer/run6Experiments-Baselines.sh
@@ -5,9 +5,15 @@ rm -rf results/cross_layer/baseline
 mkdir -p results/cross_layer/baseline/benchmarks
 
 export iteration=5
-CMDBaseline=./scripts/cross_layer/exp1_Baselines/runExperiment1-Workload-Smallbank.sh
+CMDRunSmallbank=./scripts/cross_layer/exp1_Baselines/runExperiment1-Workload-Smallbank.sh
+CMDRunTPCC=./scripts/cross_layer/exp1_Baselines/runExperiment1-Workload-TPCC.sh
 
 ### Workload: Smallbank
 ### **********
 echo '-------------------<< Running the Smallbank workload >>-------------------'
-$CMDBaseline postgres 50000 10000 smallbank
+$CMDRunSmallbank postgres 50000 10000
+
+### Workload: TPCC
+### **********
+echo '-------------------<< Running the TPCC workload >>-------------------'
+$CMDRunTPCC postgres 1 1
--- scripts/cross_layer/exp1_Baselines/runExperiment1-Workload-Smallbank.sh
+++ scripts/cross_layer/exp1_Baselines/runExperiment1-Workload-Smallbank.sh
@@ -5,7 +5,7 @@ accounts=$2
 transactions=$3
 
 exp_path="$(cd "$(dirname "$0")/../../.." && pwd)"
-
+log_fname="${exp_path}/results/cross_layer/baseline/benchmarks/runExperiment1-smallbank-${dbms}"
 
 cd "${exp_path}/workload/apps/smallbank"
 source venv/bin/activate
@@ -13,5 +13,6 @@ source venv/bin/activate
 CMD="python main.py test \
                     --driver ${dbms} \
                     --accounts ${accounts} \
-                    --transactions ${transactions}"
+                    --transactions ${transactions} \
+                    --output-path ${log_fname}"
 $CMD
