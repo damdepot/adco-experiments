b0867b1a0ec3fa822637c084801821f028b1cb50
dannykhant <dannypmkhant@gmail.com>
2026-09-02 22:57:50 +0700

    chore: sudo deps installation

--- run/query_layer/run1SetupDependencies.sh
+++ run/query_layer/run1SetupDependencies.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-apt-get update
-apt-get install libreadline-dev -y
-apt-get install libicu-dev -y
-apt-get -y install pkg-config
-apt install python3.10-venv -y
-apt-get install bison -y
-apt-get install byacc -y
-apt-get install bison flex -y
-apt-get install git make gcc -y
-apt install cmake -y
-apt-get install ninja-build build-essential make ccache pip clang -y # DuckDB
+sudo apt-get update
+sudo apt-get install libreadline-dev -y
+sudo apt-get install libicu-dev -y
+sudo apt-get -y install pkg-config
+sudo apt install python3.10-venv -y
+sudo apt-get install bison -y
+sudo apt-get install byacc -y
+sudo apt-get install bison flex -y
+sudo apt-get install git make gcc -y
+sudo apt install cmake -y
+sudo apt-get install ninja-build build-essential make ccache pip clang -y # DuckDB
