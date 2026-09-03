ae277011fa193052fe350dd854b38a2542303bbe
dannykhant <dannypmkhant@gmail.com>
2026-09-03 20:06:50 +0700

    feat: download with repo branch

--- run/query_layer/run2DownloadBaselines.sh
+++ run/query_layer/run2DownloadBaselines.sh
@@ -6,17 +6,19 @@ baseline_path="${root_path}/baselines"
 mkdir -p ${baseline_path}
 cd ${baseline_path}
 
-download() {
-    local url="$1" name="$2"
+
+download_branch() {
+    local url="$1" name="$2" branch="$3"
     echo "-------------------<< Downloading ${name} >>-------------------"
     if [ -d "${name}" ]; then
         echo "${name} already exists, skipping"
     else
-        git clone --depth 1 "${url}" "${name}"
+        git clone -b "${branch}" --depth 1 "${url}" "${name}"
     fi
 }
 
-download https://github.com/damdepot/ADCo.git ADCo
-download https://github.com/dannykhant/learnedrewrite.git LearnedRewrite
-download https://github.com/dannykhant/resequel.git ReSequel
-download https://github.com/dannykhant/r-bot.git R-Bot
+
+download_branch https://github.com/damdepot/ADCo.git ADCo develop
+download_branch https://github.com/dannykhant/learnedrewrite.git LearnedRewrite main
+download_branch https://github.com/dannykhant/resequel.git ReSequel main
+download_branch https://github.com/dannykhant/r-bot.git R-Bot main
