af66cb52d008ecbdddabcaebbddce4e89a440fb5
dannykhant <dannypmkhant@gmail.com>
2026-09-03 11:54:52 +0700

    chore: updated adco .env path for config

--- run/query_layer/run6UpdateConfigs.sh
+++ run/query_layer/run6UpdateConfigs.sh
@@ -97,6 +97,6 @@ write_apikeys baselines/R-Bot/my_rewriter/APIKeys.yaml
 write_dbconfig baselines/R-Bot/my_rewriter/DBConfig.yaml
 
 echo '-------------------<< Updating ADCo .env >>-------------------'
-write_adco_env baselines/ADCo/src/code_rewriter/.env
+write_adco_env baselines/ADCo/src/.env
 
 echo "Updated API keys and DB configs for ReSequel and R-Bot, and .env for ADCo."
